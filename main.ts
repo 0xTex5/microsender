7061d062feb61430c1fea12d50d639a9ff267c97
Fun <161741261+funfunido@users.noreply.github.com>
2025-12-02 10:24:49 -0700

    semicolons

--- main.ts
+++ main.ts
@@ -2,22 +2,22 @@
 
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
-    basic.pause(200)
-    basic.clearScreen()
+    basic.pause(200);
+    basic.clearScreen();
 })
 
-radio.setGroup(1)
-radio.setTransmitPower(1)
+radio.setGroup(1);
+radio.setTransmitPower(1);
 
-let id = 1
+let id = 1;
 
-basic.showNumber(id)
-basic.pause(2000)
-basic.clearScreen()
+basic.showNumber(id);
+basic.pause(2000);
+basic.clearScreen();
 
 basic.forever(function () {
-    radio.sendNumber(id)
-    basic.pause(200)
+    radio.sendNumber(id);
+    basic.pause(200);
 })
 
 let senderID = 2; 
