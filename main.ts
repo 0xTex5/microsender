a0c62e48b49828f521ec09199b3c558997463069
0T5 <161741261+0xTex5@users.noreply.github.com>
2025-12-03 10:01:34 -0700

    semicolon

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
  // Base code made by Git-coder-plus
 
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)  // when a number ID is sent it will detect the number that was sent to it and display it.
-    basic.pause(200) // pauses so you see the numebr for some time
-    basic.clearScreen() // clears screen (ofc)
+    basic.showNumber(receivedNumber);  // when a number ID is sent it will detect the number that was sent to it and display it.
+    basic.pause(200); // pauses so you see the numebr for some time
+    basic.clearScreen(); // clears screen (ofc)
 })
 
-radio.setGroup(1) // this is our radio group
-radio.setTransmitPower(1) // we should make a setting to change the transmit power
+radio.setGroup(1); // this is our radio group
+radio.setTransmitPower(1); // we should make a setting to change the transmit power
 
 let id = 1 // when we upload this code to the microbit use a unique ID
 
-basic.showNumber(id) // shows your id at the begenning
-basic.pause(2000) // pauses for 2 secs
-basic.clearScreen() // clears screen
+basic.showNumber(id); // shows your id at the begenning
+basic.pause(2000); // pauses for 2 secs
+basic.clearScreen(); // clears screen
 
 basic.forever(function () {
-    radio.sendNumber(id) // constnatly sends id
-    basic.pause(200) // pauses
+    radio.sendNumber(id); // constnatly sends id
+    basic.pause(200); // pauses
 })
 
 // end of base code made by me git-coder plus
